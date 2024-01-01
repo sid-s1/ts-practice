35217854aef745ca772ea04f89c0c3c30b865214
Siddharth Singh <siddharth.s1@outlook.com>
2024-01-02 08:47:08 +1100

    gathering user inputs

--- project1/dist/app.js
+++ project1/dist/app.js
@@ -1,15 +1,72 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// autobind decorator
+function autobind(_target, _methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
 class ProjectInput {
+    // selection and rough setup in the constructor, and then the insertion or fine tuning in separate methods
     constructor() {
         this.templateElement = document.getElementById("project-input");
         this.hostElement = document.getElementById("app");
         // Because as soon as this class is instantiated, we want to create our form, we will do it right here in the constructor
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        // before we attach everything, we want to get access to the different inputs in that element/form; and we want to store them as properties in the class
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.configure();
         this.attach();
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert("Invalid inpit, please try again");
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+    clearInputs() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
+    }
+    submitHandler(event) {
+        event.preventDefault(); // to prevent an HTTP request to be sent
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+        }
+        this.clearInputs();
+    }
+    configure() {
+        this.element.addEventListener("submit", this.submitHandler);
+    }
 }
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
