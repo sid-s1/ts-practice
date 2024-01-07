166b725a6f486f44ec61b4ef96a5eaafa0335171
Siddharth Singh <siddharth.s1@outlook.com>
2024-01-07 11:00:09 +1100

    setting up webpack to bundle files; next up will be to configure webpack to convert the ts files to js

--- project1/src/components/project-item.ts
+++ project1/src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Component } from './base-component.js';
-import { Project } from '../models/project.js';
-import { autobind } from '../decorators/autobind.js';
-import { Draggable } from '../models/drag-drop.js';
+import { Component } from './base-component';
+import { Project } from '../models/project';
+import { autobind } from '../decorators/autobind';
+import { Draggable } from '../models/drag-drop';
 
 // Project item class
 export class ProjectItem extends Component<HTMLUListElement,HTMLLIElement> implements Draggable {
--- project1/src/components/project-list.ts
+++ project1/src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from './base-component.js';
-import { DragTarget } from '../models/drag-drop.js';
-import { Project, ProjectStatus } from '../models/project.js';
-import { ProjectItem } from './project-item.js';
-import { autobind } from '../decorators/autobind.js';
-import { projectState } from '../state/project-state.js';
+import { Component } from './base-component';
+import { DragTarget } from '../models/drag-drop';
+import { Project, ProjectStatus } from '../models/project';
+import { ProjectItem } from './project-item';
+import { autobind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
 
 // Project list class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget{
