1a13cb0d390a43797bf211097aa79b62ef30c801
Siddharth Singh <siddharth.s1@outlook.com>
2024-01-12 09:39:49 +1100

    Creating a ts project to make a todo list - setting up the react app and some basic things

---
+++ project3/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+
+const App: React.FC = () => {
+  const todos = [{id: 't1', text: "finish the course"}];
+  return (
+    <div className="App">
+      {/* {A component that adds todos} */}
+      <TodoList items={todos}/>
+    </div>
+  );
+}
+
+export default App;
